0ff77ca54c5766b66948ed19679e69c54e928b4e
BIENFOALI TAMPO <gastontheone@live.fr>
2018-07-06 12:20:53 +0000

    création de repository pour les tables secteurs pays villes devises fournisseurs

---
+++ DAL/Repositories/PaysRepository.cs
@@ -0,0 +1,19 @@
+﻿using DAL.Models;
+using DAL.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Repositories
+{
+    public class PaysRepository : Repository<Pays>, IPaysRepository
+    {
+        public PaysRepository(DbContext context) : base(context)
+        {
+        }
+
+
+        private ApplicationDbContext _appContext => (ApplicationDbContext)_context;
+    }
+}
